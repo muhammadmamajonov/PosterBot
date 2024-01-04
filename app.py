9afeaa7ecbbfbfab347778465cbc37e36f5d6954
Muhammad <muhammadbekmamajonov@gmail.com>
2024-01-04 11:54:14 +0500

    c

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import asyncio
 from loader import bot
 from config import CHAT_ID
 
+
 async def post_and_delete():
     msg =  await bot.send_message(chat_id=CHAT_ID, text="Salom test")
     await bot.delete_message(chat_id=CHAT_ID, message_id=msg.message_id)
